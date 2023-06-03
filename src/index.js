02dd7c1079a1c8b00ccb40be6a1ef42402827a4d
Babur1992 <jon1992@ukr.net>
2023-06-03 13:33:45 +0300

    ДЗ_11

--- src/index.js
+++ src/index.js
@@ -70,8 +70,6 @@ async function searchImages(event) {
       gallery.innerHTML = images;
       loadMoreBtn.style.display = 'inline';
       loadMoreBtn.style.display = 'block';
-      // margin = ' 0 auto';
-      margin = 'left';
       border: 'none';
       padding: '15px';
       cursor: 'pointer';
